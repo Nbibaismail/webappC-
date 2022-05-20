9d27ef30ab7a226f48a64758605f8e5be4827a46
ismail nbiba <ismailnbiba@gmail.com>
2022-05-20 18:49:52 +0100

    web

---
+++ WepApp/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WepApp.Data;
+using WepApp.Models;
+
+namespace WepApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategorylist = _db.Categories;
+            return View(objCategorylist);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category was created :) ";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromdb = _db.Categories.Find(id);
+           if(categoryFromdb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromdb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category was Updated :) ";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromdb = _db.Categories.Find(id);
+            if (categoryFromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromdb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null) { return NotFound(); }
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["success"] = "Category was Deleted :) ";
+
+            return RedirectToAction("Index");
+            
+            return View(obj);
+        }
+    }
+}
---
+++ WepApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WepApp.Models;
+
+namespace WepApp.Data
+{
+    public class ApplicationDbContext  :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+        
+}
